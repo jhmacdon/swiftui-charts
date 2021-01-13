2e0097836ba9679eb5111516d0260e7f3ba659cf
Josiah MacDonald <mcdonaldjosh7@gmail.com>
2021-01-12 21:55:44 -0800

    Renaming to avoid conflict with other charts

--- Package.swift
+++ Package.swift
@@ -3,17 +3,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "Charts",
+    name: "ChartsUI",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
-        .library(name: "Charts", targets: ["Charts"])
+        .library(name: "ChartsUI", targets: ["ChartsUI"])
     ],
     dependencies: [
         .package(url: "https://github.com/swift-extensions/swiftui-shapes.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
-        .target(name: "Charts",dependencies: ["Shapes"])
+        .target(name: "ChartsUI",dependencies: ["Shapes"])
     ]
 )
